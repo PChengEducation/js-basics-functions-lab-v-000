faebc9be77dbf80cc2d7f44ba9ca70b24c1e92f3
Pan Cheng <pchengeducation@hotmail.com>
2018-08-02 22:09:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function distanceFromHqInBlocks(blocks){
   if (blocks < 44){
     return distance = 1;
   }
-  else if (distance > = 50){
+  else if (blocks > = 50){
     return distance = 8;
   }
 }
