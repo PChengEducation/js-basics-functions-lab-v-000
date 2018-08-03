b7cfe08a0f388bde48f902133e6f6b9cf09d5581
Pan Cheng <pchengeducation@hotmail.com>
2018-08-03 00:46:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function distanceFromHqInBlocks(blocks){
   let result = blocks - 42
   return result < 0? result*-1 : result
-}
+};
 function distanceFromHqlnFeet(blocks){
   return distanceFromHqlnBocks(blocks) * 12
-}
+};
