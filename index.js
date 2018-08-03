a80345697c0bc7ecdd77466fc0ebce3f7f362c56
Pan Cheng <pchengeducation@hotmail.com>
2018-08-03 01:45:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ function calculatesFarePrice(blocks, endBlock){
   let fare = 0;
   let fareCode;
   distance = distanceTravelledInFeet(blocks, endBlock);
-  switch(distance){
-    case < 264:
-    fare = 0
+  if (distance <= 264){fareCode = 1};
+  switch(fareCode){
+    case 1:
+    fare = 0;
     break;
   };
   return fare;
