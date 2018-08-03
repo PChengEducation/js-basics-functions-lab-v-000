3d684eaf5bef9192a9ceabc61998a3207fcf08b9
Pan Cheng <pchengeducation@hotmail.com>
2018-08-03 00:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blocks){
   if (blocks === 43){
-    let distance = 1
-    return distance
+    let distance = 1;
+    return distance;
   }
   else if (blocks > 43){
-    let distance = blocks - 43
-    return distance
+    let distance = blocks - 43;
+    return distance;
   }
-//  else if (blocks < 43){
-  //  let distance = 43 - blocks
-  //  return distance
-//  }
+  else if (blocks < 43){
+    let distance = 43 - blocks;
+    return distance;
+  }
 
 }
 function distanceFromHqlnFeet(blocks){
